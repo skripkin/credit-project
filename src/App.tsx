7a74d36a2374abadf227dec8b90e95725858858e
Skripkin Eugene <skripkin.eugene@gmail.com>
2021-04-23 13:39:53 +0600

    Настроил архитектуру проекта

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as Components from "./components";
+
+function App() {
+  return (
+    <>
+      <Components.Menu />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/menu/menu.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import * as Styled from "./styles";
+
+const Menu = () => {
+  return(
+    <Styled.Container>
+    </Styled.Container>
+  )
+};
+
+export default Menu;

62b75c3cc1893e5ca5a4772cbb119f89187bde78
Skripkin Eugene <skripkin.eugene@gmail.com>
2021-04-23 14:01:15 +0600

    Собрал цвета по проекту

---
+++ src/constants/colors.ts
@@ -0,0 +1,15 @@
+export const white = "#FFFFFF";
+export const black = "#000000";
+
+//Green
+export const caribbeanGreen = "#0ED193";
+
+//Yellow
+export const pear ="#CBDB2A";
+
+//Gray
+export const mystic = "#E4ECEE";
+export const towerGray = "#ACBBBE";
+export const aquaHaze = "#F0F5F6";
+
+// get colors name from https://icolorpalette.com/color/
